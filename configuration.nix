832f88de5aba7befe50048a4ee6b08f439850c09
Harry Garrood <harry@garrood.me>
2014-07-20 14:34:18 -0700

    rearranging, remove useless deploy user

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   imports =
-    [ 
+    [
       ./hardware-configuration.nix
     ];
 
@@ -18,21 +18,5 @@
     consoleFont = "lat9w-16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
-  };
-
-  services.openssh.enable = true;
-  users.extraUsers.deploy = {
-    name = "deploy";
-    group = "users";
-    extraGroups = [ "wheel" ];
-    createHome = true;
-    home = "/home/deploy";
-    useDefaultShell = true;
-    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdGXN
-2AJwq/9U5Q76jHzHww3FJE9OMjg+6JfsnjzTHHu+uLNkxMInMMuvWAnE2E1zK060ixzKLUYE5KR4nkxi
-sdHYd8zb4GmJIMcUvOJznybydCP48D60Cfr8PlSg+owJ8lLHhrZVEz3OyZQdejCRNREccE3MwEd+pYsN
-3B5K1mJRh+lCJM/Q8hH8NAaMLeYKako0mP9p000W8MSsEONX7Pe9UMctroX/VCB8C6wlHjNaOLS288Cs
-acs/XS4Yay9/8FcSDW/lMSaqFerSShNfBqPjE1xsYJIW3LnMwT+7C18rfQZ1LFFf6GZzRJq1aDrecL36
-v4yA5cL7HyjW1ah6v harry@hawk-mint " ];
   };
 }
--- roles.nix
+++ roles.nix
@@ -17,8 +17,14 @@
       networking.firewall.allowPing = true;
       networking.firewall.allowedTCPPorts = [ 80 ];
 
-      users.extraUsers = {
-        harry = { };
+      users.extraUsers.harry = {
+        name = "harry";
+        group = "users";
+        extraGroups = [ "wheel" ];
+        createHome = true;
+        home = "/home/harry";
+        useDefaultShell = true;
+        openssh.keys.authorizedKeys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdGXN2AJwq/9U5Q76jHzHww3FJE9OMjg+6JfsnjzTHHu+uLNkxMInMMuvWAnE2E1zK060ixzKLUYE5KR4nkxisdHYd8zb4GmJIMcUvOJznybydCP48D60Cfr8PlSg+owJ8lLHhrZVEz3OyZQdejCRNREccE3MwEd+pYsN3B5K1mJRh+lCJM/Q8hH8NAaMLeYKako0mP9p000W8MSsEONX7Pe9UMctroX/VCB8C6wlHjNaOLS288Csacs/XS4Yay9/8FcSDW/lMSaqFerSShNfBqPjE1xsYJIW3LnMwT+7C18rfQZ1LFFf6GZzRJq1aDrecL36v4yA5cL7HyjW1ah6v harry@hawk-mint" ];
       };
     };
 }
